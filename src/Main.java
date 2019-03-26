6344558990c2f7c8c03b9b1d70587b03f952a0f1
Chelu <joseluism@faculty.ie.edu>
2019-03-26 22:32:27 +0100

    Starting point

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import java.util.Random;
+
 public class Main {
 
+    public static int generateRandomNumbers() {
+        Random random = new Random();
+        return random.nextInt(2);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        WordAnalyzer wordAnalyzer = new WordAnalyzer("hello");
+        System.out.println(wordAnalyzer.firstRepeatedCharacter());
+        System.out.println(wordAnalyzer.countRepeatedCharacters());
+
+        wordAnalyzer = new WordAnalyzer("comprehensive");
+        System.out.println(wordAnalyzer.firstMultipleCharacter());
+        System.out.println(wordAnalyzer.firstRepeatedCharacter());
+
+        wordAnalyzer = new WordAnalyzer("development");
+        System.out.println(wordAnalyzer.firstRepeatedCharacter());
+        System.out.println(wordAnalyzer.countRepeatedCharacters());
+
+        wordAnalyzer = new WordAnalyzer("mississippi!!!");
+        System.out.println(wordAnalyzer.firstMultipleCharacter());
     }
 }
