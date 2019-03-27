5a8c4cbbe6f51ffad1c857c319723604f3ff9838
Chelu <joseluism@faculty.ie.edu>
2019-03-27 23:15:10 +0100

    Initial state

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,47 @@
+import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Random;
 
 public class Main {
 
+
+    public static final int RED_BOARD = 2000000;
+    public static final int BLUE_BOARD = 1500000;
+    static HashMap<Integer, Element> myBoard;
+
+    public static void settingTheBoard(int size) {
+        // Setting the board
+        myBoard = new HashMap<>();
+
+        for(int i = 0; i < size; i++) {
+            myBoard.put(i, new Space());
+        }
+    }
+
+    public static void minningTheBoard(int numberMines) {
+        Random random = new Random();
+        while (numberMines > 0) {
+            Integer trial = new Integer(random.nextInt(myBoard.size()));
+
+            if (myBoard.get(trial) instanceof Space) {
+                myBoard.put(trial, new Mine());
+                numberMines--;
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        WordAnalyzer wordAnalyzer = new WordAnalyzer("hello");
-        System.out.println(wordAnalyzer.firstRepeatedCharacter());
-        System.out.println(wordAnalyzer.countRepeatedCharacters());
 
-        wordAnalyzer = new WordAnalyzer("comprehensive");
-        System.out.println(wordAnalyzer.firstMultipleCharacter());
-        System.out.println(wordAnalyzer.firstRepeatedCharacter());
+        long startingTime = System.currentTimeMillis();
+        settingTheBoard(RED_BOARD);
+        minningTheBoard(1500000); //
+        long elapsedTime = (System.currentTimeMillis() - startingTime);
+        System.out.println("1,500,000 bombs planted in " + elapsedTime + " milliseconds");
 
-        wordAnalyzer = new WordAnalyzer("development");
-        System.out.println(wordAnalyzer.firstRepeatedCharacter());
-        System.out.println(wordAnalyzer.countRepeatedCharacters());
-
-        wordAnalyzer = new WordAnalyzer("mississippi!!!");
-        System.out.println(wordAnalyzer.firstMultipleCharacter());
-
-        wordAnalyzer = new WordAnalyzer("aabbcdaaaabb");
-        System.out.println(wordAnalyzer.countRepeatedCharacters());
+        startingTime = System.currentTimeMillis();
+        settingTheBoard(BLUE_BOARD);
+        minningTheBoard(1499999); //
+        elapsedTime = (System.currentTimeMillis() - startingTime);
+        System.out.println("1,499,999 bombs planted in " + elapsedTime + " milliseconds");
     }
 }
